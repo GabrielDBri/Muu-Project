81a5edc1f3385330108fa8f9d9000913f35a9fd0
Gabriel Domingos <101770912+GabrielDBri@users.noreply.github.com>
2023-11-28 06:03:48 -0300

    form

--- src/pages/RegistrarAnimais.jsx
+++ src/pages/RegistrarAnimais.jsx
@@ -1,61 +1,67 @@
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-
+import styles from '../styles/RegistrarAnimais.module.css'
+import { useState } from 'react'
 function RegistrarAnimais() {
+
+    const subirform = (event) => {
+        event.preventDefault()
+        console.log("olaa")
+    }
+
+    const [registrarState, setRegistrarState] = useState ({
+        nome: '',
+        nascimento: '',
+        raça: '',
+        genero: ''
+    })
+
+    const FormonChange = (event, key) => {
+        setRegistrarState({...registrarState, [key]: event.target.value})
+
+    }
+
     return (
-        <Form>
-        <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+        <div className={styles.divform}>
+            <form className={styles.form} onSubmit={subirform}>
+                <h1>Registrar Animal</h1>
 
-            <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-        </Row>
+                <label htmlFor="nome">Nome:
+                <input 
+                type="name" 
+                placeholder="Digite" 
+                required
+                value={registrarState.nome}
+                onChange={(event) => FormonChange(event, 'nome')} 
+                />
+                </label>
 
-        <Form.Group className="mb-3" controlId="formGridAddress1">
-            <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
-        </Form.Group>
+                <label htmlFor="nome">Nascimento:
+                <input 
+                type="date" 
+                placeholder="Nascimento" 
+                required 
+                value={registrarState.nome}
+                onChange={(event) => FormonChange(event, 'nome')} 
+                />
+                </label>
 
-        <Form.Group className="mb-3" controlId="formGridAddress2">
-            <Form.Label>Address 2</Form.Label>
-            <Form.Control placeholder="Apartment, studio, or floor" />
-        </Form.Group>
+                <label htmlFor="nome">Raça:
+                <select value={registrarState.nome} onChange={(event) => FormonChange(event, 'nome')} >
+                    <option value="opcao1">Vermelho</option>
+                    <option value="opcao2">Roxo</option>
+                </select>
+                </label>
 
-        <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
-            </Form.Group>
+                <label htmlFor="nome">Genero:
+                <select value={registrarState.nome} onChange={(event) => FormonChange(event, 'nome')} >
+                    <option value="opcao1">Macho</option>
+                    <option value="opcao2">Femea</option>
+                </select>
+                </label>
+                <button type='submit'>Enviar</button> 
 
-            <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Select defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>...</option>
-            </Form.Select>
-            </Form.Group>
+            </form>
 
-            <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control />
-            </Form.Group>
-        </Row>
-
-        <Form.Group className="mb-3" id="formGridCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-            Submit
-        </Button>
-        </Form>
+        </div>
     );
 }
 
